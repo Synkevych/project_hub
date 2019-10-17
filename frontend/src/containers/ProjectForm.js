dfb151322bcfe437a392336a7849a8c5d53fbbe2
Synkevych Roman <sunkevu4@gmail.com>
2019-10-18 00:19:36 +0300

    refactor: add new  dispatched data

--- frontend/src/containers/ProjectForm.js
+++ frontend/src/containers/ProjectForm.js
@@ -7,20 +7,45 @@ class MessageForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			message: '',
 			name: '',
-			link:'',
+			project_name: '',
+			instruction: '',
+			email: '',
+			image: '',
+			authors: '',
+			functionality: '',
+			project_goal: '',
+			link: ''
 		};
 	}
 
 	handleNewProject = event => {
 		event.preventDefault();
 		this.props.postNewMessage(this.state.message);
-		this.setState({ message: '' });
+		this.setState({
+			name: '',
+			project_name: '',
+			instruction: '',
+			email: '',
+			image: '',
+			authors: '',
+			functionality: '',
+			project_goal: '',
+			link: ''
+		});
 		this.props.history.push('/');
 	};
 	render() {
-		const {name, link } = this.state;
+		const {
+			project_name,
+			instruction,
+			email,
+			image,
+			authors,
+			functionality,
+			project_goal,
+			link
+		} = this.state;
 		return (
 			<div className='row justify-content-md-center text-center'>
 				<div className='col-md-6'>
@@ -31,71 +56,77 @@ class MessageForm extends Component {
 								{this.props.errors.message}
 							</div>
 						)}
-						<label htmlFor='name'>Назва проекту:</label>
+						<label htmlFor='project_name'>Назва проекту:</label>
 						<input
 							className='form-control'
-							id='name'
-							name='name'
-							autoComplete='on'
-							value={name}
+							id='project_name'
+							name='project_name'
+							value={project_name}
 							type='text'
+							onChange={e => this.setState({ project_name: e.target.value })}
 						/>
 						<label htmlFor='link'>Посилання на проект:</label>
 						<input
 							className='form-control'
 							id='link'
 							name='link'
-							autoComplete='on'
-							value={name}
+							value={link}
 							type='text'
+							onChange={e => this.setState({ link: e.target.value })}
 						/>
-						<label htmlFor='name'>Посилання на керівництво користувача:</label>
+						<label htmlFor='instruction'>
+							Посилання на керівництво користувача:
+						</label>
 						<input
 							className='form-control'
-							id='name'
-							name='name'
-							autoComplete='on'
-							value={name}
+							id='instruction'
+							name='instruction'
+							value={instruction}
 							type='text'
+							onChange={e => this.setState({ instruction: e.target.value })}
 						/>
-						<label htmlFor='name'>Електронна адреса:</label>
+						<label htmlFor='email'>Електронна адреса:</label>
 						<input
 							className='form-control'
-							id='name'
-							name='name'
+							id='email'
+							name='email'
 							autoComplete='on'
-							value={name}
-							type='text'
+							value={email}
+							type='email'
+							onChange={e => this.setState({ email: e.target.value })}
 						/>
-						<label htmlFor='name'>Зображення:</label>
+						<label htmlFor='image'>Посилання на зображення:</label>
 						<input
 							className='form-control'
-							id='name'
-							name='name'
-							autoComplete='on'
-							value={name}
-							type='file'
+							id='image'
+							name='image'
+							value={image}
+							type='text'
+							onChange={e => this.setState({ image: e.target.value })}
 						/>
-						<label htmlFor='name'>Автори:</label>
+						<label htmlFor='authors'>Автори:</label>
 						<textarea
 							className='form-control'
-							id='name'
-							name='name'
-							value={name}
+							id='authors'
+							name='authors'
+							value={authors}
+							onChange={e => this.setState({ authors: e.target.value })}
 						/>
-						<label htmlFor='name'>Функціональні можливості:</label>
+						<label htmlFor='functionality'>Функціональні можливості:</label>
 						<textarea
 							className='form-control'
-							id='name'
-							name='name'
-							value={name}
+							id='functionality'
+							name='functionality'
+							value={functionality}
+							onChange={e => this.setState({ functionality: e.target.value })}
 						/>
-						<label htmlFor='name'>Ціль проекту:</label>
+						<label htmlFor='project_goal'>Ціль проекту:</label>
 						<textarea
 							className='form-control'
-							id='name'
-							name='name'
-							value={name}
+							id='project_goal'
+							name='project_goal'
+							value={project_goal}
+							onChange={e => this.setState({ project_goal: e.target.value })}
 						/>
 
 						{/* <input
