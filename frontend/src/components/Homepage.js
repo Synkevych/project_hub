ccfddc8bde936cd9ece7b748dfb55985957b00d9
Synkevych Roman <sunkevu4@gmail.com>
2019-10-23 09:58:43 +0300

    fix: add new content

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -4,27 +4,29 @@ import './Homepage.css';
 import MessageTimeLine from './MessageTimeline';
 
 const Homepage = ({ currentUser }) => {
-	if (!currentUser.isAuthenticated) {
-		return (
-			<div className='home-hero'>
-				<h1>What's Happening</h1>
-				<h4>New to Ukrainian Hub Projects?</h4>
-				<Link to='/signup' className='btn btn-primary'>
-					Sign up here
-				</Link>
-			</div>
-		);
-	}
-	return (
-		<div>
-			{/* <MessageTimeLine
+  if (!currentUser.isAuthenticated) {
+    return (
+      <div className='home-hero'>
+        <h1>Віртуальний центр цифрової науки</h1>
+        <h4>Виконується в рамках Програмно-цільової і конкурсної тематики Програма
+          інформатизації НАН України{' '}
+        </h4>
+        <Link to='/signup' className='btn btn-primary'>
+          Увійти
+        </Link>
+      </div>
+    );
+  }
+  return (
+    <div>
+      {/* <MessageTimeLine
 				profileImageUrl={currentUser.user.profileImageUrl}
 				username={currentUser.user.username}
 			/> */}
-			<h1> Hi there, we glad to see you here! </h1>
-			<h4> Here information about Ukrainian Hub Projects </h4>
-		</div>
-	);
+      <h1> Вітаємо Вас на нашому сайті </h1>
+      <h4> Віртуальний центр цифрової науки </h4>
+    </div>
+  );
 };
 
 export default Homepage;
--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -1,41 +1,84 @@
 import React from 'react';
 
 const News = () => {
-	return (
-		<div className='card'>
-			<h2 className='card-header'>First post</h2>
-			<div className='card-body'>
-				<p className='card-text'>
-					<small className='text-muted'>
-						April 1, 2019 by <a href='#'>Roman</a>
-					</small>
-				</p>
-				<p>
-					Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
-					ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam
-					porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac
-					cursus commodo, tortor mauris condimentum nibh, ut fermentum massa
-					justo sit amet risus.
-				</p>
-				<ul>
-					<li>
-						Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-					</li>
-					<li>Donec id elit non mi porta gravida at eget metus.</li>
-					<li>Nulla vitae elit libero, a pharetra augue.</li>
-				</ul>
-				<p>
-					Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis
-					consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla
-					sed consectetur.
-				</p>
-				<p>
-					Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit
-					libero, a pharetra augue.
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className='card'>
+      <h2 className='card-header'>
+        Семінар 16.11.2017 &#171;Використання розподіленої обчислювальної
+        інфраструктури для підтримки наукової та науково-організаційної
+        діяльності в НАН України&#187;
+      </h2>
+      <div className='card-body'>
+        <p className='card-text'>
+          <small className='text-muted'>
+            18 лист. 2017 р., 09:12 <a href='#'>Antoniouk</a>
+          </small>
+        </p>
+        <h5>
+          Тема: «Використання розподіленої обчислювальної інфраструктури для
+          підтримки наукової та науково-організаційної діяльності в НАН України»
+        </h5>
+        <b>День проведення:</b> 16 листопада 2017р, початок о 14.00 год. <br />
+        <b>Місце проведення:</b> ауд. 218 (2-й пов.) Інституту програмних систем
+        НАН України, за адресою проспект Академіка Глушкова, 40 корпус 5.
+        <br />
+        <b>Доповідачі:</b> 1) к.т.н. Свістунов С.Я. ІТФ НАН України
+        https://drive.google.com/open?id=1FmTwjtgc8JH1S9forOoXl6ZWL0Tr98hT
+        (презентація-слайди, доповідь відбулась по слайдах №1-51).<br/> 
+		2) Святодух А.Г. ІПС НАН України
+        https://drive.google.com/open?id=1iqYuZVMOSSScImA1PR1yDJJbo0XESLgf
+        (презентація-слайди).
+        <br />
+        <br />
+        <h5>Основні питання семінару:</h5>
+        <ol>
+          <li>
+            Узагальнена схема використання розподілених ресурсів НАН України
+            (АМОД, Обчислювальний кластер, Грід ресурси, Хмарна інфраструктура)
+          </li>
+          <li>
+            Основні компоненти хмарної інфраструктури НАН України (IaaS, PaaS,
+            SaaS і їх взаємозв'язок)
+          </li>
+          <li>Сценарії роботи з хмарною інфраструктурою НАН України</li>
+          <ul>
+            <li>стандартний інтерфейс системи OpenStack dashboard,</li>
+            <li>інтерфейс OpenStack API, o інтерфейс командного рядка OCCI,</li>
+            <li>
+              програмні додатки сторонніх розробніків: пакет SlipStream, Vcycle
+            </li>
+          </ul>
+          <li>Приклади використання хмарної інфраструктури НАН України:</li>
+          <ul>
+            <li>Контекстуалізація віртуальних серверів </li>
+            <li>
+              Розподіл обчислювальних завдань з обробки даних в експериментах
+              ЦЕРН{' '}
+            </li>
+            <li>
+              Інтеграція в хмарну інфраструктуру для обробки даних експерименту
+              ALICE на LHC{' '}
+            </li>
+            <li>
+              Розгортання віртуального кластеру з використанням EC3 механізму
+            </li>
+          </ul>
+          <li>Приклади сервісів в хмарній інфраструктурі НАН України:</li>
+          <ul>
+            <li>офісні пакет на базі MS Office та на базі Libro (Open Source) </li>
+			<li>АІС Установа на розроблена на основі 1C інструментарію o системи підтримки процесів формування зведеної річної звітності НАН України</li>
+            <li>DB SPACE система баз даних наукових публікацій НАН України </li>
+			<li>Хмарне файлове сховища для організації зберігання, синхронізації та обміну даними</li>
+          </ul>
+        </ol>
+        <p>
+          <b>Організатори:</b> Перконос П.І. (ІПС НАН України), Свістунов С.Я.
+          (ІТФ НАН України), Святодух А.Г. (ІПС НАН України), Романюк Н.М. (ІМ
+          НАН України)
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default News;
